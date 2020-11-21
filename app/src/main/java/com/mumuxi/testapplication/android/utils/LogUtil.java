226c77ef7372982a29f4cc1075514f82ebf0e423
元国成 <yuanguocheng@konka.com>
2020-11-21 22:10:38 +0800

    update logUtil

--- app/src/main/java/com/mumuxi/testapplication/android/utils/LogUtil.java
+++ app/src/main/java/com/mumuxi/testapplication/android/utils/LogUtil.java
@@ -2,21 +2,25 @@ package com.mumuxi.testapplication.android.utils;
 
 import android.util.Log;
 
+import com.mumuxi.testapplication.BuildConfig;
+
 /**
- * Created by mumuxi on 2018-4-25.
- * Log工具
+ * @author mumuxi
+ * @date 2020/11/15
+ * log工具
  */
 
 public class LogUtil {
 
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = BuildConfig.DEBUG;
 
 
-    public static final boolean openLog = SystemProperties.get("test.debuggable", "false").equals
-            ("true");
+    public static final boolean openLog =
+            SystemProperties.get("test.debuggable", "false").equals("true");
 
     /**
      * 解决log信息超长，显示不完整 debug message
+     *
      * @param tag
      * @param msg
      */
@@ -43,32 +47,33 @@ public class LogUtil {
 
     /**
      * information message
+     *
      * @param tag
      * @param msg
      */
     public static void i(String tag, String msg) {
-        if (DEBUG || openLog) {
-            int strLength = msg.length();
-            int start = 0;
-            int logMaxLength = 2000;
-            int end = logMaxLength;
-            for (int i = 0; i < 100; i++) {
-                //剩下的文本还是大于规定长度则继续重复截取并输出
-                if (strLength > end) {
-                    Log.d(tag + i, msg.substring(start, end));
-                    start = end;
-                    end = end + logMaxLength;
-                } else {
-                    Log.e(tag, msg.substring(start, strLength));
-                    break;
-                }
+        int strLength = msg.length();
+        int start = 0;
+        int logMaxLength = 2000;
+        int end = logMaxLength;
+        for (int i = 0; i < 100; i++) {
+            //剩下的文本还是大于规定长度则继续重复截取并输出
+            if (strLength > end) {
+                Log.d(tag + i, msg.substring(start, end));
+                start = end;
+                end = end + logMaxLength;
+            } else {
+                Log.e(tag, msg.substring(start, strLength));
+                break;
             }
         }
+
     }
 
 
     /**
      * error message
+     *
      * @param tag
      * @param message
      */
@@ -80,6 +85,7 @@ public class LogUtil {
 
     /**
      * warning message
+     *
      * @param tag
      * @param message
      */
