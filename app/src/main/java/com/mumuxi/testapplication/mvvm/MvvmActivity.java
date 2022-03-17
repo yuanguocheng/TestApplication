8c9f0984a0e7b045f250b01cb3be7607c31b2a4d
元国成 <guocheng_yuan@aliyun.com>
2022-03-17 21:37:31 +0800

    jetpack  ViewModel使用

--- app/src/main/java/com/mumuxi/testapplication/mvvm/MvvmActivity.java
+++ app/src/main/java/com/mumuxi/testapplication/mvvm/MvvmActivity.java
@@ -2,16 +2,16 @@ package com.mumuxi.testapplication.mvvm;
 
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 
 import com.mumuxi.testapplication.R;
-import com.mumuxi.testapplication.android.utils.ToastUtils;
+import com.mumuxi.testapplication.android.utils.LogUtil;
 import com.mumuxi.testapplication.databinding.MvvmBinding;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 /**
  * @author mumuxi
@@ -19,6 +19,7 @@ import androidx.databinding.DataBindingUtil;
  */
 public class MvvmActivity extends AppCompatActivity {
 
+    public static final String TAG = MvvmActivity.class.getSimpleName();
     MvvmBinding mMvvmBinding;
     MyViewModel mMyViewModel;
     EventListener mEventListener;
@@ -26,22 +27,37 @@ public class MvvmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LogUtil.d(TAG, "onCreate");
         mMvvmBinding = DataBindingUtil.setContentView(this, R.layout.mvvm);
-        mMyViewModel = new MyViewModel();
+        mMyViewModel = new ViewModelProvider(MvvmActivity.this).get(MyViewModel.class);
+        //hashcode值没有发生变化，是同一个实例。
+        LogUtil.d(TAG, mMyViewModel.hashCode() + "");
         mEventListener = new EventListener();
         mMvvmBinding.setPerson(mMyViewModel.getPerson());
         mMvvmBinding.setEventListener(mEventListener);
-
     }
 
     public class EventListener {
 
         public void onClick(View view) {
             mMyViewModel.getPerson().setName("修改名字了");
-
         }
-
     }
 
+    @Override
+    protected void onPause() {
+        LogUtil.d(TAG, "onPause");
+        super.onPause();
+    }
 
+    @Override
+    protected void onStop() {
+        LogUtil.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
--- app/src/main/java/com/mumuxi/testapplication/mvvm/MyViewModel.java
+++ app/src/main/java/com/mumuxi/testapplication/mvvm/MyViewModel.java
@@ -1,14 +1,27 @@
 package com.mumuxi.testapplication.mvvm;
 
+import com.mumuxi.testapplication.android.utils.LogUtil;
+
+import androidx.lifecycle.ViewModel;
+
 /**
+ * ViewModel 类旨在以注重生命周期的方式存储和管理界面相关的数据
+ *
  * @author mumuxi
  * @version 2022/3/17
  */
-public class MyViewModel {
+public class MyViewModel extends ViewModel {
+
+    public static final String TAG = MyViewModel.class.getSimpleName();
 
     private Person mPerson;
 
+    /**
+     * 旋转MvvmActivity之后，构造函数并没有重新执行，
+     * 证明旋转之后 是同一个MyViewModel 实例。
+     */
     public MyViewModel() {
+        LogUtil.d(TAG, "init");
         mPerson = new Person("阿木木", 23);
     }
 
@@ -19,5 +32,11 @@ public class MyViewModel {
 
     public void setPerson(Person person) {
         mPerson = person;
+    }
+
+    @Override
+    protected void onCleared() {
+        LogUtil.d(TAG, "onCleared");
+        super.onCleared();
     }
 }
